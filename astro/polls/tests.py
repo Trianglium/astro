8a267765f027360a4ba7dc10751233244d3c85c5
Jordan <jbrockerrudow@gmail.com>
2022-10-31 15:45:38 +0000

    Add Basic page and url pattern tests

--- astro/polls/tests.py
+++ astro/polls/tests.py
@@ -6,8 +6,30 @@ from polls.models import Question, Choice
 class PollsIndexTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
+        cls.question = Question.objects.create(question_text="", pub_date="")
+        cls.choice = Choice.objects.create(question="", choice_text="", votes="")
+
+    def test_url_pattern(self):
+        response = self.client.get("/polls")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.client.get(reverse("polls-index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "polls/index.html")
 
 
 class PollsDetailTest(TestCase):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.question = Question.objects.create(question_text="", pub_date="")
+        cls.choice = Choice.objects.create(question="", choice_text="", votes="")
+
+    def test_url_pattern(self):
+        response = self.client.get("/polls/1")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.client.get(reverse("poll-details"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "polls/detail.html")
