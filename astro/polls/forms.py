efb959f0992ec777f38bcef6932978008f51106a
Jordan <jbrockerrudow@gmail.com>
2022-10-31 15:58:11 +0000

    Add PollsForm

--- astro/polls/forms.py
+++ astro/polls/forms.py
@@ -4,3 +4,14 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 from polls.models import Question, Choice
+
+
+class PollsForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+        super(PollsForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit("submit", "Submit"))
