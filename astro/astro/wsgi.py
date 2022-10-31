db80d92342e984bc54f665d45cdb91ec2cbaab9d
Jordan <jbrockerrudow@gmail.com>
2022-10-31 14:31:45 +0000

    Format with Black

--- astro/astro/wsgi.py
+++ astro/astro/wsgi.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
-path = os.path.expanduser('~/astrodev/astro/astro')
+path = os.path.expanduser("~/astrodev/astro/astro")
 if path not in sys.path:
     sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'astro.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "astro.settings"
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
+
 application = StaticFilesHandler(get_wsgi_application())
