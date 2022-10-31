ca2d9e8358a364f8c689bf684a9f84fb21857983
Jordan <jbrockerrudow@gmail.com>
2022-10-31 15:17:45 +0000

    Add Index View

--- astro/polls/views.py
+++ astro/polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Question, Choice
 
-# Create your views here.
+
+class PollsIndex(ListView):
+    template_name = "polls/index.html"
+    model = Question
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class PollDetail(DetailView):
+    pass
