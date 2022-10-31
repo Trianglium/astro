f06eb610b861fc6be6811541ba0f8fb40dd7b553
Jordan <jbrockerrudow@gmail.com>
2022-10-31 15:44:18 +0000

    Initialize Tests

--- astro/polls/tests.py
+++ astro/polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from polls.models import Question, Choice
 
-# Create your tests here.
+
+class PollsIndexTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+
+class PollsDetailTest(TestCase):
+    pass
