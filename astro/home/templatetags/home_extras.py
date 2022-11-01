0b4fa91a53307488dddb6314c4f0168843de4efc
Jordan <jbrockerrudow@gmail.com>
2022-11-01 13:21:49 +0000

    Format with black

--- astro/home/templatetags/home_extras.py
+++ astro/home/templatetags/home_extras.py
@@ -6,10 +6,6 @@ from django.utils.html import format_html
 register = template.Library()
 
 
-
-
-
-
 @register.inclusion_tag("home/current_planets.html")
 def current_planets():
     return {"title": "Current Planets"}
