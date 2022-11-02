bb9ee04da434171a681e0e2d48b61efe2badb706
Jordan <jbrockerrudow@gmail.com>
2022-11-02 16:29:59 +0000

    Format with black

--- astro/polls/urls.py
+++ astro/polls/urls.py
@@ -4,5 +4,5 @@ from .views import PollsIndex, PollDetail
 app_name = "polls"
 urlpatterns = [
     path("", PollsIndex.as_view(), name="polls-index"),
-    path("<int:id>/", PollDetail.as_view(), name="poll-detail"),
+    path("<int:question_id>/", PollDetail.as_view(), name="poll-detail"),
 ]
