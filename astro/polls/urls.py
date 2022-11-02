69e575ea27a34c71fb2c9b40e7bd37e2a1661067
Jordan <jbrockerrudow@gmail.com>
2022-11-02 18:02:25 +0000

    Add url patterns and update view mthods

--- astro/polls/urls.py
+++ astro/polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PollsIndex, PollDetail
+from .views import PollsIndex, PollDetail, PollResults, vote
 
 app_name = "polls"
 urlpatterns = [
     path("", PollsIndex.as_view(), name="polls-index"),
-    path("<int:question_id>/", PollDetail.as_view(), name="poll-detail"),
+    path("<int:pk>/", PollDetail.as_view(), name="poll-detail"),
+    path("<int:pk>/results/", PollResults.as_view(), name="poll-results"),
+    path("<int:question_id>/vote/", vote, name="poll-vote"),
 ]
--- astro/polls/views.py
+++ astro/polls/views.py
@@ -1,16 +1,46 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from .models import Question, Choice
+from django.utils import timezone
 
 
 class PollsIndex(ListView):
     template_name = "polls/index.html"
     model = Question
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 
 class PollDetail(DetailView):
     template_name = "polls/detail.html"
     model = Question
+
+
+class PollResults(DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:poll-results", args=(question.id,)))
