0252ad55b2454f5efc73fb24b5bc70a50655d2f4
Jordan <jbrockerrudow@gmail.com>
2022-11-03 17:42:35 +0000

    Fix Model Name (Resource)

--- astro/faq/models.py
+++ astro/faq/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Resources(models.Model):
+class Resource(models.Model):
     title = models.CharField(max_length=150)
     author = models.TextField()
     link = models.URLField(blank=True)
