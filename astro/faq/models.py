fad745732f63755ffc4ffed8634577d77c715c12
Jordan <jbrockerrudow@gmail.com>
2022-11-03 16:52:58 +0000

    Add Resource Model

--- astro/faq/models.py
+++ astro/faq/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Resources(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.TextField()
+    link = models.URLField(blank=True)
+
