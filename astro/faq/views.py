3e1749136311c0516bd1cfe0ec5dadab9870c27e
Jordan <jbrockerrudow@gmail.com>
2022-11-03 17:30:36 +0000

    Add Index, List and Detail views

--- astro/faq/views.py
+++ astro/faq/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import Resource
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
+
+class FAQIndex(TemplateView):
+    template_name = "faq/faq_index.html"
+
+class ResourceList(ListView):
+    model = Resource
+    template_name = "faq/resource_list.html"
+    context_object_name = "resource_list"
+
+    def get_queryset(self):
+        resources = Resource.objects.all()
+        return resources.order_by("-title")
+
+class ResourceDetail(DetailView):
+    model = Resource
+    template_name = "faq/resource_detail.html"
