23799e16c27fa17b349187e989224dbb75bb55ea
Jordan <jbrockerrudow@gmail.com>
2022-11-04 07:18:03 -0500

    Add AboutPage URL

--- astro/astro/urls.py
+++ astro/astro/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.conf import settings
 
 from django.urls import path, include
-from home.views import HomePage
+from home.views import HomePage, AboutPage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePage.as_view(), name="home"),
+    path("about/", AboutPage.as_view(), name="about"),
     path("polls/", include("polls.urls")),
     path("faq/", include("faq.urls")),
 ]
