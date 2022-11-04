e10c619893414548b4207ed9f3c59482594344e7
Jordan <jbrockerrudow@gmail.com>
2022-11-04 06:45:42 -0500

    Add About View and template

--- astro/home/views.py
+++ astro/home/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 
 class HomePage(TemplateView):
     template_name = "home/main.html"
+
+class AboutPage(TemplateView):
+    template_name: str = "home/about.html"
