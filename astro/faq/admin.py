ad133e40207499ea7521a2c7ebd1f4ed11a2d30f
Jordan <jbrockerrudow@gmail.com>
2022-11-04 12:41:08 +0000

    Register Rule Model & Migrate

--- astro/faq/admin.py
+++ astro/faq/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Resource, AstroPoint
+from .models import Resource, AstroPoint, Rule
 
 admin.site.register(Resource)
 admin.site.register(AstroPoint)
+admin.site.register(Rule)
