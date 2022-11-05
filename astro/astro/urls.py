f9932eb3a47799217b5aa405516524528a8943ca
Jordan <jbrockerrudow@gmail.com>
2022-11-05 17:37:22 +0000

    Rename url pattern

--- astro/astro/urls.py
+++ astro/astro/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("", HomePage.as_view(), name="home"),
     path("about/", AboutPage.as_view(), name="about"),
     path("polls/", include("polls.urls")),
-    path("faq/", include("faq.urls")),
+    path("learn/", include("faq.urls")),
 ]
