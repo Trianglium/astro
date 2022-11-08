0c9f6f26fbb953be7b48464ddab070429cd27d29
Jordan <jbrockerrudow@gmail.com>
2022-11-08 15:13:50 +0000

    Formatted with Black

--- astro/home/views.py
+++ astro/home/views.py
@@ -4,5 +4,6 @@ from django.views.generic.base import TemplateView
 class HomePage(TemplateView):
     template_name = "home/main.html"
 
+
 class AboutPage(TemplateView):
     template_name: str = "home/about.html"
