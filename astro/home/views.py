2f3705ba44be9816bd5585e17909f0f00d10a2cc
Jordan <jbrockerrudow@gmail.com>
2022-11-09 18:31:07 +0000

    Add Horoscopes to context

--- astro/home/views.py
+++ astro/home/views.py
@@ -7,17 +7,22 @@ class HomePage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
-        horoscopes = [pyaztro.Aztro(sign="aries"), pyaztro.Aztro(sign="taurus"), pyaztro.Aztro(sign="gemini"), pyaztro.Aztro(sign="cancer"), pyaztro.Aztro(sign="leo"), pyaztro.Aztro(sign="virgo"), pyaztro.Aztro(sign="libra"), pyaztro.Aztro(sign="scorpio"), pyaztro.Aztro(sign="sagittarius"), pyaztro.Aztro(sign="capricorn"), pyaztro.Aztro(sign="aquarius"), pyaztro.Aztro(sign="pisces")]
-        context['horoscopes'] = horoscopes
+        horoscopes = [
+            pyaztro.Aztro(sign="aries"),
+            pyaztro.Aztro(sign="taurus"),
+            pyaztro.Aztro(sign="gemini"),
+            pyaztro.Aztro(sign="cancer"),
+            pyaztro.Aztro(sign="leo"),
+            pyaztro.Aztro(sign="virgo"),
+            pyaztro.Aztro(sign="libra"),
+            pyaztro.Aztro(sign="scorpio"),
+            pyaztro.Aztro(sign="sagittarius"),
+            pyaztro.Aztro(sign="capricorn"),
+            pyaztro.Aztro(sign="aquarius"),
+            pyaztro.Aztro(sign="pisces"),
+        ]
+        context["horoscopes"] = horoscopes
         return context
-
-
-
-
-    def todaysHoroscope(self):
-        horoscope = pyaztro.Aztro(sign='aries')
-        horoscope.description
-
 
 
 class AboutPage(TemplateView):
