91681f6d48d22f2d3af020bb8a4ae555a7eb585a
Jordan <jbrockerrudow@gmail.com>
2022-11-11 14:43:56 +0000

    Register Tags Model with Admin

--- astro/faq/admin.py
+++ astro/faq/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Resource, AstroPoint, Rule
+from .models import Resource, AstroPoint, Rule, Tag
 
 admin.site.register(Resource)
 admin.site.register(AstroPoint)
 admin.site.register(Rule)
+admin.site.register(Tag)
