a54e7ba192b21e71f42d7a59d021233c355d7006
Jordan <jbrockerrudow@gmail.com>
2022-11-12 16:01:42 +0000

    Add Basic Document model

--- astro/reader/models.py
+++ astro/reader/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    title = models.TextField()
+    author = models.TextField(blank=True, default="unknown")
+    pages = models.PositiveIntegerField(blank=True)
+    cover = models.ImageField(upload_to="documents/", blank=True)
+
+    def __str__(self):
+        return f"{self.title}, {self.author}"
