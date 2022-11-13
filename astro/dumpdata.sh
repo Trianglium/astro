fd210dca97191b9bf4744e5389f70411d4d99680
Jordan <jbrockerrudow@gmail.com>
2022-11-13 15:17:35 +0000

    Add Tag Model

--- astro/dumpdata.sh
+++ astro/dumpdata.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 function dumpData() {
-    python manage.py dumpdata -o data.json faq.AstroPoint faq.Rule faq.Resource polls.Question polls.Choice auth.User;
+    python manage.py dumpdata -o data.json faq.AstroPoint faq.Tag faq.Rule faq.Resource polls.Question polls.Choice auth.User;
 }
 
 dumpData
