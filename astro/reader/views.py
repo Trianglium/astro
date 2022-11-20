71c67392c57ec57527054e8d0d8450f3512a16a6
Jordan <jbrockerrudow@gmail.com>
2022-11-20 13:06:21 -0600

    Add List & Detail Views

--- astro/reader/views.py
+++ astro/reader/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from .models import Document
+
+
+class DocIndex(ListView):
+    template_name = "reader/doc.html"
+    model = Document
+
+class DocDetail(DetailView):
+    template_name = "reader/page.html"
+    model = Document
