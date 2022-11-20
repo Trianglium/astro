4b4875f3d790d9027cbd85c858a2f7e7ad69e482
Jordan <jbrockerrudow@gmail.com>
2022-11-20 13:05:55 -0600

    Add Doc Detail Tests

--- astro/reader/tests.py
+++ astro/reader/tests.py
@@ -18,4 +18,15 @@ class DocIndexTests(TestCase):
 
 
 
+class DocDetailTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.doc = Document.objects.create(title="", author="", pages="", cover="")
+    def test_url_pattern(self):
+        response = self.client.get("/read/1")
+        self.assertEqual(response.status_code, 200)
 
+    def test_index_page(self):
+        response = self.client.get(reverse("doc-detail"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reader/page.html")
