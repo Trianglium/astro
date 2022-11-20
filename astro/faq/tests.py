d4f51163a9a51f3b3352a7efc877c7f99119012f
Jordan <jbrockerrudow@gmail.com>
2022-11-20 13:03:41 -0600

    Add FAQIndex Tests

--- astro/faq/tests.py
+++ astro/faq/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from faq.models import Resource, AstroPoint
+
+class FAQIndexTests(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.astro_point = AstroPoint.objects.create()
+
+    def test_url_pattern(self):
+        response = self.client.get("/learn")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.client.get(reverse("faq-index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "faq/faq_index.html")
 
 
