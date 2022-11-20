cc2bdda76048b48336d57efeb4a774ec9f036bdc
Jordan <jbrockerrudow@gmail.com>
2022-11-20 13:03:55 -0600

    Add Home Page Tests

--- astro/home/tests.py
+++ astro/home/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
+class HomeTests(TestCase):
+    def test_url_pattern(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/main.html")
