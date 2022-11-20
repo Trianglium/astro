90569ed701de48bb73cb7871feeb0af8219c533c
Jordan <jbrockerrudow@gmail.com>
2022-11-20 12:55:37 -0600

    Initialize Forum Tests (WIP)

--- astro/forum/tests.py
+++ astro/forum/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from forum.models import Comment, Tag, Post
 
-# Create your tests here.
+class ForumIndexTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.comment = Comment.objects.create()
+        cls.tag = Tag.objects.create()
+        cls.post = Post.objects.create()
+
+    def test_url_pattern(self):
+        response = self.client.get("/forum")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.client.get(reverse("forum-index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "forum/index.html")
+
+
+
+
