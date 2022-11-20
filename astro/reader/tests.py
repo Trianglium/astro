dbaf8b145ea414fae488d3a45632969ad85b8910
Jordan <jbrockerrudow@gmail.com>
2022-11-20 13:05:16 -0600

    Add DocIndex Tests

--- astro/reader/tests.py
+++ astro/reader/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from reader.models import Document
+
+class DocIndexTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.doc = Document.objects.create(title="", author="", pages="", cover="")
+
+    def test_url_pattern(self):
+        response = self.client.get("/read")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.client.get(reverse("doc-index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reader/doc.html")
+
+
 
 
