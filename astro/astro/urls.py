1076ff8cd2b131a502a895f95512480e3602b109
Jordan <jbrockerrudow@gmail.com>
2022-12-19 16:55:01 +0000

    Formatted with Black

--- astro/astro/urls.py
+++ astro/astro/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from home.views import HomePage, AboutPage
 
 urlpatterns = [
-    #path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("", HomePage.as_view(), name="home"),
     path("about/", AboutPage.as_view(), name="about"),
     path("polls/", include("polls.urls")),
